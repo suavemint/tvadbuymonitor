bbb8511a5150fe4314afc4afce834a462ef7037f
James Orcutt <ej.orcutt@gmail.com>
2014-07-11 13:05:14 -0400

    Fixed watcher-watching relationship; should be functional now. Also added a date_added property to the Watcher class.

--- flask/app/models.py
+++ flask/app/models.py
@@ -8,16 +8,29 @@ class User(db.Model):
     name = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     role = db.Column(db.SmallInteger, default=ROLE_USER)
-    watching = db.relationship('Watcher', backref='',lazy='dynamic')
+    watching = db.relationship('Watcher', backref='watched',lazy='dynamic')
 
     def __repr__(self):
         return '<User %r>' % self.name
 
 # This db is the source file, from which users' queries will be polled at
 # + set interval(s).
+
+# Should probably work a selection for each user, by DMA of interest, for an update
+# + check.
+
+# We'll start with a single-market check.
+
+
+# Add a Watcher item with the syntax: w = models.Watcher(market='Omaha', watched=u)
 class Watcher(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    number = db.Column(db.SmallInteger, unique=True)
+    market = db.Column(db.String(40), unique=True)
+    watch = db.Column(db.Integer, db.ForeignKey('user.id'))
+    date_added = db.Column(db.Date)
+
+    def __repr__(self):
+        return '<Watcher %r by %r>' % (self.market, self.watch)
 
 
 class AdBuys(db.Model):
@@ -36,4 +49,10 @@ class AdBuys(db.Model):
     total_spent_raw = db.Column(db.Integer)
     num_spots_raw = db.Column(db.Integer)
     contract_number = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<AdBuy: %s; %s; %s; %s; %s; %s; %s; %s; %s; %s; %s; %s; %s; %s; %s>' % \
+        (self.id, self.station, self.file_upload_date, self.contract_start_date, self.contract_end_date,\
+         self.tv_market, self.tv_market_id, self.ad_type, self.fcc_folder, self.fcc_file_name, self.is_invalid,\
+         self.is_invoice, self.total_spent_raw, self.num_spots_raw, self.contract_number)
 
